5f16d51b57674dbfebc2d4a58b8cc670c4f30e79
tunahansalih <tusame1994>
2018-11-06 02:18:27 +0300

    image stitching done

--- homography.py
+++ homography.py
@@ -3,111 +3,134 @@ import cv2
 import pickle
 import matplotlib.pyplot as plt
 
-selected_points = pickle.load(open('tmp/selected_points.p', 'rb'))
-im1Points = np.array(selected_points[0])
-im2Points = np.array(selected_points[1])
-
-
-def normalize(points):
-    mean_of_images = np.mean(points, 1)
-    dist_of_points = np.sum(np.sqrt(np.sum(np.square(points), axis=2)), axis=1)
-    print(dist_of_points)
-    print(mean_of_images)
-    for i in range(len(points)):
-        points[i] = points[i] - mean_of_images[i]
-    return points / dist_of_points * np.sqrt(2)
-
 
 def normalization_matrix(points):
+    print('creating normalization matrix')
     nm = np.diag(np.ones(3))
     mean_of_points = np.mean(points, 0)
-    dist_of_points = np.sum(np.square(np.sum(np.square(points), 1)))
+    dist_of_points = np.sum(np.sqrt(np.sum(np.square(points), 1))) / len(points)
     nm[0, 2] -= mean_of_points[0]
     nm[1, 2] -= mean_of_points[1]
     nm[:2, :] /= dist_of_points
+    nm[:2, :] *= np.sqrt(2)
     return nm
 
 
 def create_A_matrix_columns(im1point, im2point):
+    print('creating A matrix column')
     col1 = np.array(
-        [0, 0, 0, -im1point[0], -im1point[1], -1, im2point[1] * im1point[0], im1point[1] * im2point[1], im2point[1]])
-    col2 = np.array(
         [im1point[0], im1point[1], 1, 0, 0, 0, -im2point[0] * im1point[0], -im2point[0] * im1point[1], -im2point[0]])
+    col2 = np.array(
+        [0, 0, 0, im1point[0], im1point[1], 1, -im2point[1] * im1point[0], -im2point[1] * im1point[1], -im2point[1]])
     cols = np.stack((col1, col2))
     return cols
 
 
 def create_A_matrix(im1Points, im2Points):
+    print('Creating A matrix')
     A = []
     for i in range(len(selected_points[0])):
         cols = create_A_matrix_columns(im1Points[i], im2Points[i])
+
         A.append(cols)
 
     return np.vstack(A)
 
 
-def computeH(im1Points, im2Points):
-    im1_normalization_matrix = normalization_matrix(im1Points)
-    im2_normalization_matrix = normalization_matrix(im2Points)
-    im1Points_normalized = (im1_normalization_matrix @ np.insert(im1Points, 2, 1, axis=1).T).T
-    im2Points_normalized = (im2_normalization_matrix @ np.insert(im2Points, 2, 1, axis=1).T).T
-    u, s, vh = np.linalg.svd(create_A_matrix(im1Points_normalized, im2Points_normalized))
-    normalized_homography_matrix = np.reshape(vh[-1, :] / vh[-1, -1], (3, 3))
-    return np.linalg.inv(im2_normalization_matrix) @ normalized_homography_matrix @ im1_normalization_matrix
+def computeH(im1Points, im2Points, normalize=True):
+    print('Computing homography matrix')
+    if normalize:
+        im1_normalization_matrix = normalization_matrix(im1Points)
+        im2_normalization_matrix = normalization_matrix(im2Points)
+        im1Points_normalized = np.matmul(im1_normalization_matrix, np.insert(im1Points, 2, 1, axis=1).T).T
+        im2Points_normalized = np.matmul(im2_normalization_matrix, np.insert(im2Points, 2, 1, axis=1).T).T
+        A = create_A_matrix(im1Points_normalized, im2Points_normalized)
+        u, s, vh = np.linalg.svd(A)
+        normalized_homography_matrix = np.reshape(vh[-1, :], (3, 3))
+        homography_matrix = np.matmul(np.matmul(np.linalg.inv(im2_normalization_matrix), normalized_homography_matrix),
+                                      im1_normalization_matrix) / normalized_homography_matrix[-1, -1]
+    else:
+        u, s, vh = np.linalg.svd(
+            create_A_matrix(np.insert(im1Points, 2, 1, axis=1), np.insert(im2Points, 2, 1, axis=1)))
+        homography_matrix = np.reshape(vh[-1, :], (3, 3))
+    return homography_matrix / homography_matrix[2, 2]
 
 
 def warp(image, H):
-    new_coordinates = np.zeros((image.shape[0], image.shape[1], 2))
-    min_x, min_y, max_x, max_y = 0, 0, 0, 0
+    print('Warping')
+
+    y, x = np.meshgrid(np.arange(image.shape[1]), np.arange(image.shape[0]))
+    coordinate_matrix = np.array([np.concatenate(np.stack((x, y), axis=2))],dtype=np.float)
+    new_coordinates = np.reshape(cv2.perspectiveTransform(coordinate_matrix, H), (image.shape[0], image.shape[1], 2))
+
+    new_img = np.zeros(BLENDED_IMAGE_SIZE)
+    left_x_min_coordinate = int((BLENDED_IMAGE_SIZE[0] - image.shape[0]) / 2)
+    left_y_min_coordinate = int((BLENDED_IMAGE_SIZE[1] - image.shape[1]) / 2)
     for x in range(image.shape[0]):
         for y in range(image.shape[1]):
-            new_coordinate = (H @ np.array([x, y, 1]).T).astype(np.int)
-            min_x = min(min_x, new_coordinate[0])
-            max_x = max(max_x, new_coordinate[0])
-            new_coordinates[x, y, :] = new_coordinate[:2]
-
-    new_img = np.zeros((max_x - min_x + 1, image.shape[1], 3))
-    for x in range(image.shape[0]):
-        for y in range(image.shape[1]):
-            new_coordinate = (new_coordinates[x, y] - [min_x, min_y]).astype(np.int)
-            new_img[new_coordinate[0], new_coordinate[1]] = image[x, y]
-
-    # if 0 <= new_coordinate[0] < image.shape[0] and 0 <= new_coordinate[1] < image.shape[1]:
-    #     new_image[int(new_coordinate[0]), int(new_coordinate[1])] = image[x, y]
-
-    return new_img.astype(np.uint8), min_x, max_x
+            new_coordinate = new_coordinates[x, y] + [left_x_min_coordinate, left_y_min_coordinate]
+            new_img[int(new_coordinate[0]), int(new_coordinate[1])] = image[x, y]
+    return new_img.astype(np.uint8)
 
 
 def interpolate(new_image):
+    print('Interpolating')
     mask = np.array(np.sum(new_image == 0, axis=2), dtype=np.uint8)
     interpolated_image = cv2.inpaint(src=new_image, inpaintMask=mask, inpaintRadius=5, flags=cv2.INPAINT_NS)
     return interpolated_image
 
 
-def blend(img1, img2, min_x, max_x):
-    blended_image = np.zeros((max(img2.shape[0] - min_x, max_x - min_x), max(img2.shape[1] - min_y, max_y - min_y), 3))
+def expand(img):
+    new_img = np.zeros(BLENDED_IMAGE_SIZE)
+    left_x_min_coordinate = int((BLENDED_IMAGE_SIZE[0] - img2.shape[0]) / 2)
+    left_y_min_coordinate = int((BLENDED_IMAGE_SIZE[1] - img2.shape[1]) / 2)
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            new_img[x + left_x_min_coordinate, y + left_y_min_coordinate] = img[x, y]
+    return new_img
+
+
+def blend(img1, img2):
+    print('blending')
+    blended_image = np.zeros(BLENDED_IMAGE_SIZE)
+    img1_intensity = np.sum(img1, axis=2) / 3
+    img2_intensity = np.sum(img2, axis=2) / 3
     for x in range(blended_image.shape[0]):
         for y in range(blended_image.shape[1]):
-            if img2.shape[1] - min_x > x > -min_x and img2.shape[1] - min_y > y > -min_y:
-                blended_image[x, y, :] = img2[x + min_x, y + min_y, :]
-            elif x < max_x - min_x - 1 and y < max_y - min_y - 1:
-                blended_image[x, y, :] = img1[x, y, :]
-
+            if img1_intensity[x, y] > img2_intensity[x, y]:
+                blended_image[x, y] = img1[x, y]
+            else:
+                blended_image[x, y] = img2[x, y]
     return blended_image
 
 
-homography_matrix = computeH(im1Points, im2Points)
+files = ['images/input/left-2.jpg',
+         'images/input/left-1.jpg',
+         'images/input/middle.jpg',
+         'images/input/right-1.jpg',
+         'images/input/right-2.jpg', ]
+BLENDED_IMAGE_SIZE = [2000, 4000, 3]
 
+selected_points = pickle.load(open('tmp/selected_points.p', 'rb'))
+# im1Points = np.array(selected_points[0])
+# im2Points = np.array(selected_points[1])
+
+im1Points = np.flip(selected_points[1], axis=1)
+im2Points = np.flip(selected_points[2], axis=1)
+
+homography_matrix = computeH(im1Points, im2Points)
+print(homography_matrix - cv2.findHomography(im1Points, im2Points)[0])
 img1 = plt.imread("images/input/left-1.jpg")
 img2 = plt.imread("images/input/middle.jpg")
 
-new_img1, (min_x, min_y), (max_x, max_y) = warp(img1, homography_matrix)
-
+new_img1 = warp(img1, homography_matrix)
+# new_img1 = cv2.warpPerspective(img1, homography_matrix, (img1.shape[1], img1.shape[0]))
+# new_img1 = interpolate(new_img1)
 plt.imshow(new_img1)
 plt.show()
-new_img1 = interpolate(new_img1)
-plt.imshow(new_img1)
-plt.show()
-blended = blend(new_img1, img2, min_x, min_y, max_x, max_y)
-plt.imshow(blended)
-plt.show()
+print(img1.shape)
+#
+# new_img2 = expand(img2).astype(np.int)
+# blended = blend(new_img1, new_img2).astype(np.int)
+# plt.imshow(blended)
+# plt.show()
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import pickle
 
-selected_points = []
+NUM_OF_INTEREST_POINTS = 12
+files = ['images/input/left-2.jpg',
+         'images/input/left-1.jpg',
+         'images/input/middle.jpg',
+         'images/input/right-1.jpg',
+         'images/input/right-2.jpg', ]
+selected_points = np.zeros((len(files), NUM_OF_INTEREST_POINTS, 2))
+images = []
 
-img = plt.imread("images/input/left-1.jpg")
-plt.imshow(img)
-selected_points.append(plt.ginput(8, show_clicks=True))
-print('clicked', selected_points)
-plt.close()
+for f in files:
+    img = plt.imread(f)
+    images.append(img)
 
-img = plt.imread("images/input/middle.jpg")
-plt.imshow(img)
-selected_points.append(plt.ginput(8, show_clicks=True))
-print('clicked', selected_points)
-plt.close()
+
+for i in range(12):
+    for j, img in enumerate(images):
+        plt.imshow(img)
+        clicked = plt.ginput(1, show_clicks=True, timeout=0)
+        selected_points[j,i,:] = np.array(clicked)
+        print(clicked)
+        plt.close()
+
 
 pickle.dump(selected_points, open('tmp/selected_points.p', 'wb'))
